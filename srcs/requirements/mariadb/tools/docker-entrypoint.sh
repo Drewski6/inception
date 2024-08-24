8dc2247636ee76cf8aa20f98ea6d00b00155cfdd
Drew <91377105+Drewski6@users.noreply.github.com>
2024-08-24 18:10:47 +0200

    refactor: Update docker-compose.yml to use specific versions of nginx and mariadb images also added config for mariadb-server.cnf added some notes to the readme in the docker-entrypoint.sh changed some print comments, and added some setup for the mariadb db

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-fdate() {
-	echo $(date '+%Y-%m-%d %H:%M:%S')
+stamp() {
+	echo "$(date '+%Y-%m-%d %H:%M:%S') 0 [info] inception:"
 }
 
-echo "$(fdate) [info] Starting MariaDB"
+echo "$(stamp) Starting MariaDB"
 
 #############################################################
 # NOTE                                                      #
@@ -15,28 +15,64 @@ echo "$(fdate) [info] Starting MariaDB"
 
 # Create /run/mysqld directory and set owner to mysql user.
 if [ -d "/run/mysqld" ]; then
-	echo "$(fdate) [info] mysqld already present, skipping creation"
+	echo "$(stamp) mysqld already present, skipping creation"
 	chown -R mysql:mysql /run/mysqld
 else
-	echo "$(fdate) [info] mysqld not found, creating...."
+	echo "$(stamp) mysqld not found, creating...."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
 # Create /var/lib/mysql directory and set owner to mysql user.
 if [ -d /var/lib/mysql/mysql ]; then
-	echo "$(fdate) [info] MariaDB directory already present, skipping creation"
+	echo "$(stamp) MariaDB directory already present, skipping creation"
 	chown -R mysql:mysql /var/lib/mysql
 else
-    echo "$(fdate) [info] MariaDB data directory not found, creating initial DBs"
+    echo "$(stamp) MariaDB data directory not found, creating initial DBs"
 	chown -R mysql:mysql /var/lib/mysql
 
     mysql_install_db --user=mysql --datadir=/var/lib/mysql # > /dev/null
 
-    echo "$(fdate) [info] MariaDB root password: $MYSQL_ROOT_PASSWORD"
-    echo "$(fdate) [info] MariaDB password: $MYSQL_PASSWORD"
-    echo "$(fdate) [info] MariaDB user: $MYSQL_USER"
-    echo "$(fdate) [info] MariaDB database: $MYSQL_DATABASE"
+    echo "$(stamp) MariaDB root password: $MYSQL_ROOT_PASSWORD"
+    echo "$(stamp) MariaDB password: $MYSQL_PASSWORD"
+    echo "$(stamp) MariaDB user: $MYSQL_USER"
+	echo "$(stamp) MariaDB database: $MYSQL_DATABASE"
+
+	tempfile=`mktemp`
+	if [ ! -f "$tempfile" ]; then
+	    return 1
+	fi
+
+	cat << EOF > $tempfile
+USE mysql;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+DELETE FROM mysql.user WHERE User='';
+FLUSH PRIVILEGES ;
+EOF
+
+	if [ "$MYSQL_DATABASE" != "" ]; then
+	    echo "$(stamp) Creating database: $MYSQL_DATABASE"
+		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
+			echo "$(stamp) with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tempfile
+		else
+			echo "$(stamp) with character set: 'utf8' and collation: 'utf8_general_ci'"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tempfile
+		fi
+
+		if [ "$MYSQL_USER" != "" ]; then
+			echo "$(stamp) Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+			echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tempfile
+		fi
+	fi
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tempfile
+	rm -f $tempfile
+	echo "$(stamp) MySQL init process done. Ready for start up."
+
 fi
 
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
